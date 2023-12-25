9f24b194be816b77c70d34b405fd0b4cb2491775
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-26 01:46:30 +0800

    refactoring #2

---
+++ sherevo.rb
@@ -0,0 +1,39 @@
+require_relative 'config'
+
+class Sherevo
+  include Config
+
+  def initialize
+    sherevo
+  end 
+
+  private 
+
+  def content
+    @content ||= File.readlines(absolute_path).shuffle
+  end
+
+  def absolute_path
+    project_dir + '/' + sherevo_file
+  end
+
+  def project_dir
+    ENV['HOME'] + '/' + PROJECT_FOLDER 
+  end
+
+  def sherevo_file
+    SHEREVO_FILE
+  end
+
+  def sherevo_of_the_day
+    "\n#{content.sample}\n"
+  end
+
+  def random_color
+    String.colors.sample
+  end
+
+  def sherevo
+    puts sherevo_of_the_day.colorize(color: random_color, mode: :bold)
+  end
+end 
